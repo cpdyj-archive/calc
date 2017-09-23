6012823922599cbb3fedcb0dd418478745297a3d
cpdyj <canpodeyiji@gmail.com>
2017-09-23 23:06:52 +0800

    分词未完成，下次需要加入对变量token的支持。 当前支持：double int +-*/() function(,)（尚未完成）。

---
+++ src/Parser.java
@@ -0,0 +1,79 @@
+
+import java.util.*;
+import java.util.regex.*;
+/*
+constructor: Init and check the source String is legal.
+getTokens: Parse the string to tokens list.
+					Use regex to split the string to a token string.
+					Call getTk to parse the token string to a token object.
+					At the end of return the tokens list.
+getTk: Parse the token string to a Token object.
+*/
+public class Parser{
+	String source;
+	public Parser(String src){
+		if(src == null){
+			throw new NullPointerException("source string is null!");
+		}
+		source = src.trim();
+	}
+	public ArrayList<Token> getTokens(String src){
+		Pattern token_pattern=Pattern.compile("\\d+(\\.\\d+)?|\\+|\\-|\\*|\\/|\\(|\\)|[A-z]+ *\\(|,\n");
+		Matcher matcher=token_pattern.matcher(src);
+		ArrayList<Token> tks=new ArrayList<Token>();
+		//use regex split the string.
+		while(matcher.find()){
+			//getTk return the Token object , add it to list.
+			tks.add(getTk(matcher.group()));
+		}
+		return tks;
+	}
+	private Token getTk(String s){
+		Token tk=new Token();
+		int ch=s.charAt(0);
+		//if parser ok, return immediately.
+		//Operater
+		switch(ch){
+			case '+':
+				tk.type = Token.ADD;
+				return tk;
+			case '-':
+				tk.type = Token.SUB;
+				return tk;
+			case '*':
+				tk.type = Token.MUL;
+				return tk;
+			case '/':
+				tk.type = Token.DIV;
+				return tk;
+			case '(':
+				tk.type = Token.LBK;
+				return tk;
+			case ')':
+				tk.type = Token.RBK;
+				return tk;
+		}
+		//Function
+		//if the first character is a letter.
+		if((ch > 64 & ch < 91) | (ch > 96 & ch < 123)){
+			//TODO
+		}
+		try{
+			//Integer
+			//if not find '.'
+			if(s.indexOf('.') != -1){
+				tk.i = Integer.parseInt(s);
+				tk.type = Token.INTEGER;
+			}else{
+				//or it  maybe a double
+				tk.d = Double.parseDouble(s);
+				tk.type = Token.DOUBLE;
+			}
+		}catch(NumberFormatException ex){
+			//TODO may throw it. Don't pass it.
+		}
+		return tk;
+	}
+	ArrayList<Token> tokens;
+
+}
---
+++ src/Token.java
@@ -0,0 +1,16 @@
+public class Token{
+	public static final int INTEGER=1;
+	public static final int DOUBLE=2;
+	public static final int ADD=3;
+	public static final int SUB=4;
+	public static final int MUL=5;
+	public static final int DIV=6;
+	public static final int FUNC=7;
+	public static final int LBK=8;
+	public static final int RBK=9;
+	public int type;
+	public int i;
+	public double d;
+	public String fn;
+
+}
